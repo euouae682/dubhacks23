f5d60f2233b0ee50b622461926a89ca488356f06
RedPandaGaminq <timothyzhong2@gmail.com>
2023-10-14 13:36:09 -0700

    LYAOUT

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,11 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
-  return <p>Hello WOrld</p>;
+  return (
+    <main className="grid grid-cols-10 h-screen">
+      <div className="bg-cyan-300">NAV BAR</div>
+      <div className="col-span-3">HUMAN DIAGRAM</div>
+      <div className="col-span-6 bg-zinc-300">MAIN</div>
+    </main>
+  );
 }
