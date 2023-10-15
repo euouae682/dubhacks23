1445486d91d9fb0e1cedd8392637a66c95c50929
euouae682 <jasonuzhao682@gmail.com>
2023-10-14 22:12:15 -0700

    finished the scheduleitem component THANK GOD

--- app/components/main_components/hourtable.tsx
+++ app/components/main_components/hourtable.tsx
@@ -1,52 +1,29 @@
+'use client';
+
+import { ScheduleItem } from "./scheduleitem"
+import { useState } from "react"
+
 export function HourTable() {
+  const [beginTime, setBeginTime] = useState(new Date('2023-10-14T10:00'));
+
     return (
       <div className="bg-orange-200 rounded-xl p-5">
         <h3 className="font-bold tracking-wide text-orange-900">
           Your Schedule
         </h3>
         <div className="m-5 grid grid-cols-12 grid-rows-3 relative">
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25">&nbsp;</div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
-          <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
+          { Array.apply(null, Array(48)).map(function () {}).map(() => {
+            return <div className="h-12 border-dashed border-l-2 border-orange-800 border-opacity-25"></div>
+          })}
           <div className="col-span-2 mt-2 -ml-5 text-sm font text-orange-800">10:00 am</div>
           <div className="col-span-2 mt-2 -ml-5 text-sm font text-orange-800">11:00 am</div>
           <div className="col-span-2 mt-2 -ml-5 text-sm font text-orange-800">12:00 pm</div>
           <div className="col-span-2 mt-2 -ml-5 text-sm font text-orange-800">1:00 pm</div>
           <div className="col-span-2 mt-2 -ml-5 text-sm font text-orange-800">2:00 pm</div>
           <div className="col-span-2 mt-2 -ml-5 text-sm font text-orange-800">3:00 pm</div>
+          <ScheduleItem color="bg-orange-700" name="Blood Draw" location="Floor 3, Room 311" begin={new Date('2023-10-14T10:40')} end={new Date('2023-10-14T12:40')} scheduleBegin={beginTime} zAlign={0} />
+          <ScheduleItem color="bg-orange-600" name="Web Development Hell" location="MGH 342" begin={new Date('2023-10-14T11:30')} end={new Date('2023-10-14T14:55')} scheduleBegin={beginTime} zAlign={1} />
+          <ScheduleItem color="bg-orange-600" name="Salvation" location="freedom" begin={new Date('2023-10-14T14:00')} end={new Date('2023-10-14T15:30')} scheduleBegin={beginTime} zAlign={2} />
         </div>
       </div>
     )
