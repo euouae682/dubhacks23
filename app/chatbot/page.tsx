0828e2c9ac2992afa1d4e39251bc3cf7cfffa0b6
RedPandaGaminq <timothyzhong2@gmail.com>
2023-10-15 07:03:15 -0700

    ADDED CHATBOX TO NAV

---
+++ app/chatbot/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function SignIn() {
+  return (
+    <div className="flex h-screen">
+      <img src="chatbot-ui.png" />
+    </div>
+  );
+}
--- app/navbar.tsx
+++ app/navbar.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { useRouter } from "next/navigation";
 
@@ -7,7 +7,7 @@ export function Navbar() {
 
   return (
     <div className=" fixed flex flex-col items-center -ml-5 gap-10 text-xl">
-      <a href="#" className="group" onClick={() => router.push('/')}>
+      <a href="#" className="group" onClick={() => router.push("/")}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -19,7 +19,7 @@ export function Navbar() {
         </svg>
       </a>
 
-      <a href="#" className="group" onClick={() => router.push('/stats')}>
+      <a href="#" className="group" onClick={() => router.push("/stats")}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -27,6 +27,21 @@ export function Navbar() {
           className="w-10 h-10 group-hover:fill-orange-100 transition"
         >
           <path d="M18.375 2.25c-1.035 0-1.875.84-1.875 1.875v15.75c0 1.035.84 1.875 1.875 1.875h.75c1.035 0 1.875-.84 1.875-1.875V4.125c0-1.036-.84-1.875-1.875-1.875h-.75zM9.75 8.625c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v11.25c0 1.035-.84 1.875-1.875 1.875h-.75a1.875 1.875 0 01-1.875-1.875V8.625zM3 13.125c0-1.036.84-1.875 1.875-1.875h.75c1.036 0 1.875.84 1.875 1.875v6.75c0 1.035-.84 1.875-1.875 1.875h-.75A1.875 1.875 0 013 19.875v-6.75z" />
+        </svg>
+      </a>
+
+      <a href="#" className="group" onClick={() => router.push("/chatbot")}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="white"
+          className="w-10 h-10 group-hover:fill-orange-100 transition"
+        >
+          <path
+            fillRule="evenodd"
+            d="M4.848 2.771A49.144 49.144 0 0112 2.25c2.43 0 4.817.178 7.152.52 1.978.292 3.348 2.024 3.348 3.97v6.02c0 1.946-1.37 3.678-3.348 3.97a48.901 48.901 0 01-3.476.383.39.39 0 00-.297.17l-2.755 4.133a.75.75 0 01-1.248 0l-2.755-4.133a.39.39 0 00-.297-.17 48.9 48.9 0 01-3.476-.384c-1.978-.29-3.348-2.024-3.348-3.97V6.741c0-1.946 1.37-3.68 3.348-3.97z"
+            clipRule="evenodd"
+          />
         </svg>
       </a>
     </div>
